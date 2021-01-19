300a30af7187d3bcb974ce70b8cb641fcd12b9bd
jikisheree <srikeaw2020@gmail.com>
2021-01-19 16:31:13 +0700

    Finish all

--- lab12_1.cpp
+++ lab12_1.cpp
@@ -17,3 +17,29 @@ int main(){
     cout << "\nMin = " << B[5];
     return 0;
 }
+
+void stat(const double a[],int n,double b[]){
+    double sum=0, sum2=0, times=1, sumh=0, max=-999, min=999;
+    for(int i=0 ; i<n ; i++){
+        sum += a[i];
+    }
+    b[0] = sum/n;
+    for(int i=0; i<n ; i++){
+        sum2 += pow(a[i] - (sum/(double)n),2);
+    }
+    b[1] = sqrt(sum2/(double)n);
+    for(int i=0 ; i<n ; i++){
+        times *= a[i];
+    }
+    b[2] = pow(times,1/(double)n);
+    for(int i=0 ; i<n ; i++){
+        sumh += 1/a[i];
+    }
+    b[3] = (double)n/sumh;
+    for(int i=0 ; i<n ; i++){
+        if(a[i] > max) max = a[i];
+        if(a[i] < min) min = a[i];
+    }
+    b[4] = max;
+    b[5] = min;
+}
